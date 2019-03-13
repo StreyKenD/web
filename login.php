8dd1e2958a864ba2a1b89f7447515f62521350e1
StreyKenD <44182358+StreyKenD@users.noreply.github.com>
2019-03-13 12:20:07 -0300

    Update login.php

--- login.php
+++ login.php
@@ -7,17 +7,11 @@
     <title>titulo</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
 
 </head>
 <style>
     body{
-        background-color: #ececf6;
+        background-color: #d7d7e1;
     }
     div#interface{
         background-color:#c8cbcf;
@@ -28,30 +22,42 @@
         border-radius:20px;
     }
 </style>
+<body>
 <div id="interface">
-<form class="login" method="get" action="logando.php">
-    <fieldset class="login">
-        <div class="form-group">
-            Usuario:<input class="form-control" type="text" name="login" maxlength="16" placeholder="Usuário"><br/>
-        </div>
-        <div class="form-group">
-            Senha:<input class="form-control" type="password" name="password" placeholder="Senha"/><br/>
-        </div>
+    <form class="login" method="post" action="logando.php">
+        <fieldset class="login">
             <div class="form-group">
-            <div class="form-check">
-            <!--<input type="checkbox" class="form-check-input" id="checkbox">Lembre-se de mim-->
-        </div>
-    </div>
-    <input class="btn btn-primary" type="submit" value="login">
-</form>
-<div class="dropdown-divider"></div>
+                Usuario:<input class="form-control" type="text" name="login" maxlength="16" placeholder="Usuário"><br/>
+            </div>
+            <div class="form-group">
+                Senha:<input class="form-control" type="password" name="password" placeholder="Senha"/><br/>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <!--<input type="checkbox" class="form-check-input" id="checkbox">Lembre-se de mim-->
+                </div>
+            </div>
+            <?php
+            session_start();
+            if(! empty($_SESSION['incpass'])){
+                echo'Senha incorreta</br>';
+            }
+            if(! empty($_SESSION['incnom'])){
+                echo'Nome incorreto</br>';
+            }
+            ?>
+            <input class="btn btn-primary" type="submit" value="login">
+    </form>
+    <div class="dropdown-divider"></div>
     <!--<a class="dropdown-item" href="">Esqueceu a senha?</a>-->
 
-    <form method="get" action="registro.php">
+    <form method="post" action="registro.php">
         <!--<input class="btn btn-primary" type="submit" value="Cadastrar-se"/> Botao antigo-->
-    <a class="dropdown-item" href="registro.php">Novo por aqui? Registre-se</a>
-</form>
+        <a class="dropdown-item" href="registro.php">Novo por aqui? Registre-se</a>
+    </form>
     </fieldset>
 </div>
+}
+
 </body>
 </html>
